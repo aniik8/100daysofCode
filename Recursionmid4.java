4abb8b57077c72597d19cc45c9e666c2698d8e4c
Aniket <aniketmessi3@gmail.com>
2023-01-07 21:01:05 +0530

    adding substring question

--- Recursionmid4.java
+++ Recursionmid4.java
@@ -1,6 +1,9 @@
 public class Recursionmid4 {
     public static void main(String[] args) {
+        //1. skipping a character from a string
         System.out.println(skipaChar("baccad", 'a', 0));
+        //2. writing different substring of a given string
+        substring("abc", "");
     }
 
     static String skipaChar(String sc, char ch, int len){
@@ -13,5 +16,14 @@ public class Recursionmid4 {
         }
     }
 
+    // 2.
+    static void substring(String sc, String left){
+        if (sc.length()==0) {
+            System.out.println(left);
+            return;
+        }
+        substring(sc.substring(1), left+sc.charAt(0));
+        substring(sc.substring(1), left);
+    }
 
 }
