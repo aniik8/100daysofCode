7b8e6281f18bb270f16666da64ca198c67a1f659
Aniket <aniketmessi3@gmail.com>
2023-01-11 23:28:24 +0530

    Added : whether an array is ascending

--- RecursionQmid.java
+++ RecursionQmid.java
@@ -4,12 +4,21 @@ public class RecursionQmid {
     public static void main(String[] args) {
         // 1. To check whether an array is ascending or not using recursion.
         int[] arr = {12,18,23,45,68,98,122};
+        int[] arr2 = {2, 1, 1, 1, 1,0, 1, 2, 3,4,5,6};
 
+        // Counting a particular element of an array.
         asc_or_not(arr, 0, arr.length-1);
+        System.out.println(count_the_occurance(arr2, 1));
     }
 
     static boolean asc_or_not(int[] arr, int start, int end){
         if(arr.length <=1) return true;
         else return asc_or_not(Arrays.copyOfRange(arr, 0, arr.length/2-1), 0, arr.length/2 -1) && (arr[end/2-1] <= arr[end/2]) && asc_or_not(Arrays.copyOfRange(arr, end/2-1, arr.length/2), end/2, end-1);
     }
+    // counting a particular element of an array
+    static int count_the_occurance(int[] x, int n){
+        if(x.length == 0) return 0;
+        else return ((x[0] == n) ? 1 : 0) + count_the_occurance(Arrays.copyOfRange(x, 1, x.length), n);
+    }
+
 }
