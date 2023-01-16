7a11db2680a8905e4c127366185635b6c04c1b0e
Aniket <aniketmessi3@gmail.com>
2023-01-17 00:19:47 +0530

    implemented rabbit problem using mutual recursion

--- mutualRecursion.java
+++ mutualRecursion.java
@@ -1,6 +1,9 @@
 public class mutualRecursion {
     public static void main(String[] args) {
         System.out.println(isEven(8));
+        
+        // rabbit problem using mutual recursion
+        System.out.print(baby_rabbit(4));
     }
 
     // number odd or even using mutual recursion
@@ -11,6 +14,16 @@ public class mutualRecursion {
     static boolean isEven(int n){
         if(n == 0) return true;
         else return isOdd(n-1);
+    }
+    // rabbit problem using mutual recursion
+    static int adult_rabbit(int n){
+        if(n == 1) return 0;
+        else return adult_rabbit(n-1) + baby_rabbit(n-1);
+    }
+
+    static int baby_rabbit(int n) {
+        if(n == 1) return 1;
+        else return adult_rabbit(n-1);
     }
 }
 
