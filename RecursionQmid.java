985d21c08d182adfe34c3086b81c1bfa0c0bb722
Aniket <aniketmessi3@gmail.com>
2023-01-18 23:23:23 +0530

    Function to remove adjacent duplicates characters from a string

--- RecursionQmid.java
+++ RecursionQmid.java
@@ -65,4 +65,31 @@ public class RecursionQmid {
             tower_of_hanoi_leetcode(n-1);
         }
     }
+    //
+    // Function to remove adjacent duplicates characters from a string
+        public static String removeDuplicates(String s)
+        {
+            // base case
+            if (s == null) {
+                return null;
+            }
+
+            char[] chars = s.toCharArray();
+            char prev = 0;
+            int k = 0;
+
+            for (char c: chars)
+            {
+                if (prev != c)
+                {
+                    chars[k++] = c;
+                    prev = c;
+                }
+            }
+
+            return new String(chars).substring(0, k);
+        }
+
+
+    }
 }
