6e814b3b7e7078ec50fd59c3dd5623117133370b
Aniket <aniketmessi3@gmail.com>
2023-01-21 00:05:42 +0530

    Backtracking

--- backtracking/MazeQ.java
+++ backtracking/MazeQ.java
@@ -8,6 +8,8 @@ public class MazeQ {
                 {true, false, true},
                 {true, true, true}};
         pathObstacles("", 0, 0, board);
+        allpathObstacles("", 0, 0, board);
+
     }
     static int count_maze(int row, int column){
         if(row == 1 || column == 1) return 1;
@@ -52,5 +54,20 @@ public class MazeQ {
         if(row < maze.length-1) pathObstacles( p + 'R', row+1, column, maze);
         if(column < maze[0].length-1) pathObstacles( p + 'D', row, column+1, maze);
 
+    }
+    // all paths included using BackTracking
+    static void allpathObstacles(String p, int row, int column, boolean[][] maze){
+        if((row == maze.length - 1) && (column == maze[0].length-1) ){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[row][column]) return;
+        maze[row][column] = false;
+        if(row < maze.length-1) allpathObstacles( p + 'R', row+1, column, maze);
+        if(column < maze[0].length-1) allpathObstacles( p + 'D', row, column+1, maze);
+        if(row > 0) allpathObstacles(p+'U', row-1, column, maze);
+        if(column > 0) allpathObstacles(p+'L', row, column-1, maze);
+        maze[row][column] = true;
     }
 }
