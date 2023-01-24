d9fda31718d6b3bf8209419a5d9f20ba79511587
Aniket <aniketmessi3@gmail.com>
2023-01-24 23:56:54 +0530

    deletion of middle node

--- LinkedList/Doubly_LL.java
+++ LinkedList/Doubly_LL.java
@@ -82,7 +82,14 @@ public class Doubly_LL {
     }
     // will make a function that'll return a value/node at desired index and then we'll search or delete that..
     public void deleteMid(int index){
-
+        int count = 0;
+        Node temp = head;
+        while(count != index){
+            temp = temp.next;
+            count++;
+        }
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
     }
 
     Node head;
