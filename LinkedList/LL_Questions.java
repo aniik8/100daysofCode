fc936d33ccebf8fe0b71e03573af330ff794debb
Aniket <aniketmessi3@gmail.com>
2023-01-28 22:40:55 +0530

    Leetcode question 21

--- LinkedList/LL_Questions.java
+++ LinkedList/LL_Questions.java
@@ -37,5 +37,24 @@ public class LL_Questions {
             }
         }
     }
+    public LinkedList.LL.Node mergeTwoLists(LinkedList.LL.Node list1, LinkedList.LL.Node list2) {
+        LinkedList.LL.Node newlist = new LinkedList.LL.Node();
+        LinkedList.LL.Node head = newlist;
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        while(list1 != null && list2 != null){
+            if(list1.data < list2.data){
+                head.next = list1;
+                list1 = list1.next;
+                head = head.next;
+            }else{
+                head.next = list2;
+                list2 = list2.next;
+                head = head.next;
+            }
+            head.next = (list1 != null) ? list1 : (list2 != null ? list2 : null);
+        }
+        return newlist.next;
+    }
 
 }
