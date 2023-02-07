273f00cd46c5c2d10eb9d87d9d8d9d639bcc5c6b
Aniket <aniketmessi3@gmail.com>
2023-02-07 23:40:26 +0530

    Basic Queues

---
+++ Stacks&Queues/QueuesClass.java
@@ -0,0 +1,24 @@
+package StacksNQueues;
+
+public class QueuesClass {
+    public static void main(String[] args) {
+
+    }
+}
+class QueueClass{
+    private int maxLength;
+    private int top;
+    private int[] queueArray;
+    private int front;
+    private int rear;
+
+    QueueClass(int maxLength){
+        queueArray = new int[maxLength];
+        front = 0;
+        rear = -1;
+
+    }
+
+
+
+}
