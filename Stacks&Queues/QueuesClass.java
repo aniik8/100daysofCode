33ae193f5830510d84538013e45f07e6f3c9d2d6
Aniket <aniketmessi3@gmail.com>
2023-02-12 00:59:28 +0530

    leetcode 651

--- Stacks&Queues/QueuesClass.java
+++ Stacks&Queues/QueuesClass.java
@@ -210,5 +210,26 @@ class MyQueue {
         if(!(sc.isEmpty())) return false;
         return true;
     }
+    public boolean checkValidStrings(String s) {
+        Stack<Integer> stBracket = new Stack<>();
+        Stack<Integer> stStar = new Stack<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {stBracket.push(i);}
+            else if(s.charAt(i) == '*') {stStar.push(i);}
+            else {
+                if(!stBracket.isEmpty()) {stBracket.pop();} // found matched '('with')'
+                else if (!stStar.isEmpty()) {stStar.pop();} // match '*' with ')'
+                else return false; // cannot find match
+            }
+        }
+
+        // checking leftover on 2 stack
+        while(!stBracket.isEmpty()) { // we can have '*' leftover, but 'stBracket' must run out
+            if(stStar.isEmpty()) {return false;} // '(' leftover
+            else if (stBracket.peek()<stStar.peek()){stBracket.pop(); stStar.pop();} // '*'index > '(' index, matched
+            else {return false;} // stBracket>top, corner case
+        }
+        return true;
+    }
 }
