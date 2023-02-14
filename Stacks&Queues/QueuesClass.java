1845e3a552949e38026cdbedeb4b3e64fc978b5e
Aniket <aniketmessi3@gmail.com>
2023-02-14 23:43:06 +0530

    leetcode 1523

--- Stacks&Queues/QueuesClass.java
+++ Stacks&Queues/QueuesClass.java
@@ -237,8 +237,8 @@ class MyQueue {
 }
 class Solution {
     public int countOdds(int low, int high) {
-        if(low % 2 != 0 ){
-            return ((high - low) / 2)+ 1;
-        }else return (high - low) / 2;
+        if(low % 2 == 0) low = low+1;
+        if(high % 2 == 0) high = high - 1;
+        return (((high - low)/2)+1);
     }
 }
