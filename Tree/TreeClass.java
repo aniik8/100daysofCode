446cb3054f1b4166f351a644e070d7862879de2d
Aniket <aniketmessi3@gmail.com>
2023-02-15 00:58:35 +0530

    insertion in a tree (bst)

--- Tree/TreeClass.java
+++ Tree/TreeClass.java
@@ -7,3 +7,32 @@ class Node{
     Node leftChild;
     Node rightChild;
 }
+class tree{
+    Node root;
+    public void insertNode(int data){
+        Node newNode = new Node();
+        Node parent;
+        newNode.data = data;
+        if(root == null)
+            root = newNode;
+        else{
+            Node current = newNode;
+            while(true){
+                parent = current;
+                if(data < current.data){
+                    current = current.leftChild;
+                    if(current == null){
+                        parent.leftChild = newNode;
+                        return;
+                    }
+                }else{
+                    current = current.rightChild;
+                    if(current == null) {
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
