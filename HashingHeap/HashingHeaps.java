3b51392671e35adabc0d3bff8fc9e4f53e4b29c3
Aniket <aniketmessi3@gmail.com>
2023-02-26 23:31:53 +0530

    implementing hashing basics

--- HashingHeap/HashingHeaps.java
+++ HashingHeap/HashingHeaps.java
@@ -1,4 +1,14 @@
 package HashingHeap;
-
+import java.util.*;
 public class HashingHeaps {
+    public static void main(String[] args) {
+        HashSet<Integer> hash = new HashSet<>(100);
+        hash.add(4);
+        hash.add(10);
+        hash.add(19);
+        hash.add(25);
+        System.out.println(hash);
+        System.out.println(hash.size());
+    }
+
 }
