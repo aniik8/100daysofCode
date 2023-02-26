3eda3564458b83f12891d9ca9f5afa3f506c9310
Aniket <aniketmessi3@gmail.com>
2023-02-26 23:59:33 +0530

    intersection of two array (count version)

--- HashingHeap/HashingHeaps.java
+++ HashingHeap/HashingHeaps.java
@@ -10,8 +10,9 @@ public class HashingHeaps {
         System.out.println(hash);
         System.out.println(hash.size());
     }
+
     // count distinct element in an array using hashmap
-    static int countDistinct(int[] arr){
+    static int countDistinct(int[] arr) {
         int count = 0;
         HashSet<Integer> hashsEt = new HashSet<>(arr.length);
         for (int a :
@@ -20,15 +21,34 @@ public class HashingHeaps {
         }
         return hashsEt.size();
     }
+
     // union of two arrays
-    static int unionOfArray(int[] arr, int[] arr1){
-        HashSet<Integer> hashSet = new HashSet<>(arr.length+arr1.length);
-        for(int a: arr){
+    static int unionOfArray(int[] arr, int[] arr1) {
+        HashSet<Integer> hashSet = new HashSet<>(arr.length + arr1.length);
+        for (int a : arr) {
             hashSet.add(a);
         }
-        for(int a: arr1){
+        for (int a : arr1) {
             hashSet.add(a);
         }
         return hashSet.size();
+    }
+
+    // intersection of two arrays.
+    public static int intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        int[] arr = new int[Math.min(nums1.length, nums2.length)];
+        int count = 0, i = 0;
+        for (int a :
+                nums1) {
+            hashSet.add(a);
+        }
+        for (int a :
+                nums2) {
+            if (hashSet.contains(a)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
