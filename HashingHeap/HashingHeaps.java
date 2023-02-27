e7b32150b88e1f03cf6191e6c9a4d9cd2da6f3a7
Aniket <aniketmessi3@gmail.com>
2023-02-28 00:00:10 +0530

    // find kth maximum/minimum in an array

--- HashingHeap/HashingHeaps.java
+++ HashingHeap/HashingHeaps.java
@@ -96,4 +96,19 @@ public class HashingHeaps {
 
         }
     }
+    // find kth maximum/minimum in an array
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < k; i++){
+            pq.add(nums[i]);
+        }
+        for(int i = k; i < nums.length; i++){
+            if(pq.peek() < nums[i]){
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+        return pq.peek();
+    }
 }
