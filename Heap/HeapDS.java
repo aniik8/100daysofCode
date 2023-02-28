e169f390fd2b050e886462f8a9324ad4da8ab07a
Aniket <aniketmessi3@gmail.com>
2023-02-28 23:56:45 +0530

    Heap Insertion

--- Heap/HeapDS.java
+++ Heap/HeapDS.java
@@ -1,4 +1,52 @@
 package Heap;
 
 public class HeapDS {
+    static void heapify(int[] arr, int n, int i)
+    {
+        // Find parent
+        int parent = (i - 1) / 2;
+
+        if (arr[parent] > 0) {
+            // For Max-Heap
+            // If current node is greater than its parent
+            // Swap both of them and call heapify again
+            // for the parent
+            if (arr[i] > arr[parent]) {
+
+                // swap arr[i] and arr[parent]
+                int temp = arr[i];
+                arr[i] = arr[parent];
+                arr[parent] = temp;
+
+                // Recursively heapify the parent node
+                heapify(arr, n, parent);
+            }
+        }
+    }
+
+    // Function to insert a new node to the heap.
+    static int insertNode(int[] arr, int n, int Key)
+    {
+        // Increase the size of Heap by 1
+        n = n + 1;
+
+        // Insert the element at end of Heap
+        arr[n - 1] = Key;
+
+        // Heapify the new node following a
+        // Bottom-up approach
+        heapify(arr, n, n - 1);
+
+        // return new size of Heap
+        return n;
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int[] arr, int n)
+    {
+        for (int i = 0; i < n; ++i)
+            System.out.println(arr[i] + " ");
+
+        System.out.println();
+    }
 }
