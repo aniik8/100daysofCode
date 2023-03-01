0bac6cf6e56130e6bae980d6ef2ecc70754fcb90
Aniket <aniketmessi3@gmail.com>
2023-03-01 22:34:59 +0530

    Leetcode 1 Two sum

--- Arrays/ArrayDS.java
+++ Arrays/ArrayDS.java
@@ -1,7 +1,30 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class ArrayDS {
     public static void main(String[] args) {
         // Two sum leetcode Questions.
+        // 1. Two Sum bruteforce Solution
+        int[] nums = {3,3};
+        System.out.println(Arrays.toString(twoSum(nums, 6)));
+
+
+
     }
+    public static int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++){
+                if(target - nums[i] == nums[j]){
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
+            }
+        }
+        return arr;
+    }
+
 }
+
