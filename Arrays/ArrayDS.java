c069f46f54976ef6c9a4db2125b468ef0edf688c
Aniket <aniketmessi3@gmail.com>
2023-03-03 01:41:53 +0530

    2053. Kth Distinct String in an Array

--- Arrays/ArrayDS.java
+++ Arrays/ArrayDS.java
@@ -64,6 +64,26 @@ public class ArrayDS {
         }
         return pq.peek();
     }
-
+//2053. Kth Distinct String in an Array
+// 2053 leetcode
+public static String kthDistinct(String[] arr, int k) {
+    HashMap<String, Integer> map = new HashMap<>();
+    for(int i = 0; i < arr.length; i++){
+        if(map.containsKey(arr[i])){
+            map.put(arr[i], map.get(arr[i])+1);
+        }
+        else{
+            map.put(arr[i], 1);
+        }
+    }
+    int distinctkey = 0;
+    for (int i = 0; i < arr.length; i++) {
+        if(map.get(arr[i]) == 1)
+            distinctkey++;
+        if(distinctkey == k)
+            return arr[i];
+    }
+    return "";
+}
 }
 
