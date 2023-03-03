18bcf4ae1dddeeeed14852efe7219911abb47124
Aniket <aniketmessi3@gmail.com>
2023-03-04 00:03:40 +0530

    TwoSum Optimized

--- Arrays/ArrayDS.java
+++ Arrays/ArrayDS.java
@@ -28,6 +28,20 @@ public class ArrayDS {
         }
         return arr;
     }
+    // Two sum optimized
+    // https://leetcode.com/problems/two-sum/
+    public static int[] twoSum2(int[] nums, int target) {
+        int[] arr = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                arr[0] = i;
+                arr[1] = map.get(target - nums[i]);
+                return arr;
+            }else map.put(nums[i], i);
+        }
+        return arr;
+    }
     public static int KthDistinct(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
