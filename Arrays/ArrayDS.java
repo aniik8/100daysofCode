976fd4bf5c4431a1ff49c558c0cdc931d3a0db51
Aniket <aniketmessi3@gmail.com>
2023-03-04 00:04:16 +0530

    //  217 contains duplicates

--- Arrays/ArrayDS.java
+++ Arrays/ArrayDS.java
@@ -99,5 +99,15 @@ public static String kthDistinct(String[] arr, int k) {
     }
     return "";
 }
+    //  217 contains duplicates
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i]))
+                return true;
+            else map.put(nums[i], 1);
+        }
+        return false;
+    }
 }
 
