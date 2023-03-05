2baedfa7d461c221ad160524d23e8a8240cb4f94
Aniket <aniketmessi3@gmail.com>
2023-03-05 22:42:14 +0530

    1095 LEETCODE

--- BinarySearch.java
+++ BinarySearch.java
@@ -130,4 +130,67 @@ public static int findPeakElement(int[] nums) {
         }
         return copy;
     }
+    //1095 LEETCODE
+    public static int findInMountainArray(int target, int[] arr) {
+        int start=0; int end = arr.length-1;
+
+        while (start <=end){
+            int mid = (start + end)/2;
+            if(start >= arr.length-1){
+                return start;
+            }
+            if(arr[mid] > arr[mid+1]){
+                end = mid -1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        int peak = start;
+        int num  = binarysearch(arr, target, 0, peak);
+        if(num != -1) return num;
+        else {
+            return binarySearchTwo(arr, peak+1,  arr.length - 1, target);
+        }
+
+    }
+    public static int binarysearch(int[] arr, int ele, int start, int end){
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(ele == arr[mid]){
+                return mid;
+            }
+            else if(ele > arr[mid]){
+                start = mid + 1;
+            }
+            else if(ele < arr[mid]){
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    static int binarySearchTwo(int arr[], int start, int end, int x)
+    {
+
+        boolean isAsc = arr[start] < arr[end];
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+            if (arr[middle] == x)
+                return middle;
+            if (isAsc == true) {
+                if (arr[middle] < x)
+                    start = middle + 1;
+                else
+                    end = middle - 1;
+            }
+
+            else {
+                if (arr[middle] > x)
+                    start = middle + 1;
+                else
+                    end = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
 }
