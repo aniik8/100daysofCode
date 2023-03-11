a3737b41078e46fa2a2352da62aaa096bee180b8
Aniket <aniketmessi3@gmail.com>
2023-03-11 21:24:01 +0530

    219. Contains Duplicate II

--- BinarySearch.java
+++ BinarySearch.java
@@ -431,15 +431,18 @@ class Solutions {
     }
     static boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        boolean decision = false;
-        for (int i = 0; i < nums.length; i++) {
-            if(map.containsKey(nums[i])) {
-                decision = Math.abs(map.get(nums[i]) - i) <= k;
-                map.put(nums[i], i);
+        for(int i=0;i<nums.length;i++){
+
+            if(map.containsKey(nums[i])){
+                if(Math.abs(map.get(nums[i])-i)<=k)return true;
+                else map.put(nums[i],i);
+
             }
-            else
-                map.put(nums[i], i);
+            else{
+                map.put(nums[i],i);
+            }
+
         }
-        return decision;
+        return false;
     }
 }
