4f5fb862e921f99c9f69a9807f6a6fcff6b32e23
Aniket <aniketmessi3@gmail.com>
2023-03-12 20:45:53 +0530

    1903. Largest Odd Number in String

--- BinarySearch.java
+++ BinarySearch.java
@@ -484,4 +484,12 @@ class Solutions {
         }
         return count;
     }
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if(((int)num.charAt(i)) % 2 != 0)
+                return num.substring(0, i+1);
+
+        }
+        return "";
+    }
 }
