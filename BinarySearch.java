b749fdc6fcbb21ebde1ab6532311ab7d073f970b
Aniket <aniketmessi3@gmail.com>
2023-03-12 20:36:12 +0530

    560. Subarray Sum Equals K

--- BinarySearch.java
+++ BinarySearch.java
@@ -445,7 +445,7 @@ class Solutions {
         }
         return false;
     }
-    // sub array size equal to k
+    // sub array size equal to k for positive number
     static int subarraySum(int[] nums, int k) {
         int i = 0, j = 0;
         int sum = 0, count = 0;
@@ -467,6 +467,20 @@ class Solutions {
                 }
                 j++;
             }
+        }
+        return count;
+    }
+    // actual solution of subarray sum = k problem
+    static int subarraysSum(int[] nums, int k) {
+        int sum = 0, count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(map.containsKey(sum - k)){
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0)+1);
         }
         return count;
     }
