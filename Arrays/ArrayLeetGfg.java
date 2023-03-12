d1a2def32bebfbee105c8dd9241f04ef1223389b
Aniket <aniketmessi3@gmail.com>
2023-03-12 23:09:00 +0530

    Sliding window median

---
+++ Arrays/ArrayLeetGfg.java
@@ -0,0 +1,56 @@
+package Arrays;
+
+import java.util.*;
+
+public class ArrayLeetGfg {
+    public static void main(String[] args) {
+
+        int[] nus = {2147483647,1,2,3,4,5,6,7,2147483647};
+        System.out.println(Arrays.toString(medianSlidingWindow(nus, 2)));
+//        System.out.println(Integer.MAX_VALUE);
+    }
+    static double[] medianSlidingWindow(int[] nums, int k) {
+
+        double[] result;
+        if(k % 2 != 0) result = ifKodd(nums, k);
+        else result = ifKeven(nums, k);
+        return result;
+    }
+    static double[] ifKodd(int[] nums, int k){
+        int i = 0, j = 0, num = 0;
+        double[] result = new double[nums.length - k + 1];
+        int[] temp = new int[k];
+        while(j < nums.length){
+            if((j - i + 1) < k){
+                j++;
+            }
+            else if((j - i) + 1 == k){
+                temp = Arrays.copyOfRange(nums, i, j+1);
+                Arrays.sort(temp);
+
+//                System.out.println(Math.ceil((i+j)/2));
+                result[num++] = (double)temp[k/2];
+                i++;j++;
+            }
+        }
+        return result;
+    }
+    static double[] ifKeven(int[] nums, int k){
+        int i = 0, j = 0, num = 0;
+        double[] result = new double[nums.length - k + 1];
+        int[] temp = new int[k];
+        while(j < nums.length){
+            if((j - i + 1) < k){
+                j++;
+            }
+            else if((j - i) + 1 == k){
+                temp = Arrays.copyOfRange(nums, i, j+1);
+                Arrays.sort(temp);
+//                System.out.println(Math.ceil((i+j)/2));
+                result[num++] =((double)(temp[k/2])/2) + ((double)(temp[(k/2) - 1])/2);
+                i++;j++;
+            }
+        }
+        return result;
+    }
+}
--- BinarySearch.java
+++ BinarySearch.java
@@ -492,4 +492,5 @@ class Solutions {
         }
         return "";
     }
+
 }
