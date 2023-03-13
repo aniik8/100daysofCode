19258b1a13027063910aced6f634255e6f6c0861
Aniket <aniketmessi3@gmail.com>
2023-03-13 23:59:17 +0530

    lergest odd array

--- Arrays/ArrayLeetGfg.java
+++ Arrays/ArrayLeetGfg.java
@@ -68,4 +68,12 @@ public class ArrayLeetGfg {
         }
         return max_sum;
     }
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if(((int)num.charAt(i)) % 2 != 0)
+                return num.substring(0, i+1);
+
+        }
+        return "";
+    }
 }
