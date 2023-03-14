de99a0c5f523815b6d7a7e0dfb601f04514f9404
Aniket <aniketmessi3@gmail.com>
2023-03-14 23:48:46 +0530

    152 second best approach

--- Arrays/ArrayLeetGfg.java
+++ Arrays/ArrayLeetGfg.java
@@ -99,4 +99,30 @@ public class ArrayLeetGfg {
         }
         return max_product;
     }
+
+    // 2nd approach : best intuition
+
+        public int maxProductBest(int[] nums) {
+
+            int n = nums.length;
+            int l=1,r=1;
+            int ans=nums[0];
+
+            for(int i=0;i<n;i++){
+
+                //if any of l or r become 0 then update it to 1
+                l = l==0 ? 1 : l;
+                r = r==0 ? 1 : r;
+
+                l *= nums[i];   //prefix product
+                r *= nums[n-1-i];    //suffix product
+
+                ans = Math.max(ans,Math.max(l,r));
+
+            }
+
+            return ans;
+
+        }
+
 }
