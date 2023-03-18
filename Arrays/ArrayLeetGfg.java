f64f562996ffcf627f2bc47d39b8f6d97c61d191
Aniket <aniketmessi3@gmail.com>
2023-03-18 23:41:40 +0530

    GFG --> Longest K unique characters substring

--- Arrays/ArrayLeetGfg.java
+++ Arrays/ArrayLeetGfg.java
@@ -304,5 +304,35 @@ return "";
         }
         return arr;
     }
+// GFG --> Longest K unique characters substring  -- google amazon sp labs
+
+    static int longestkSubstr(String s, int k) {
+        // code here
+        int max = -1,  i = 0, j = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(j < s.length()) {
+            if (map.containsKey(s.charAt(j)))
+                map.put(s.charAt(j), map.get(s.charAt(j)) + 1);
+            else map.put(s.charAt(j), 1);
+
+            if (map.size() < k)
+                j++;
+            else if (k == map.size()) {
+                max = Math.max(max, j - i + 1);
+                j++;
+            } else if (map.size() > k) {
+                while (map.size() > k) {
+                    map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
+                    if (map.get(s.charAt(i)) == 0)
+                        map.remove(s.charAt(i));
+                    i++;
+
+                }
+                j++;
+            }
+        }
+
+        return max;
+    }
 }
 
