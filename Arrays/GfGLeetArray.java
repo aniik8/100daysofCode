e2f97ae20da7836f8ad36c64d98b8feafdbb57c0
Aniket <aniketmessi3@gmail.com>
2023-03-25 22:57:44 +0530

    Search in nearly sorted array

--- Arrays/GfGLeetArray.java
+++ Arrays/GfGLeetArray.java
@@ -89,5 +89,22 @@ static int findKRotation(int arr[], int n) {
         }
         return -1;
     }
-
+    // additional Question and good one
+    // Search in nearly sorted array - that is like elements in an array are either at i - 1, i or i+1 position.
+    static int searchInnearly(int[] arr, int target){
+        int start = 0, end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }if(mid - 1 >= start && arr[mid-1] == target)
+                return mid - 1;
+            if(mid + 1 <= end && arr[mid+1] == target)
+                return mid + 1;
+            else if(arr[mid] > target)
+                end = mid - 2;
+            else start = mid + 2;
+        }
+        return -1;
+    }
 }
