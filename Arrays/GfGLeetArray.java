40ccd45a7ada5d034056967773b1549ce64a63de
Aniket <aniketmessi3@gmail.com>
2023-03-25 23:53:10 +0530

    744. Find Smallest Letter Greater Than Target

--- Arrays/GfGLeetArray.java
+++ Arrays/GfGLeetArray.java
@@ -149,4 +149,25 @@ static int findKRotation(int arr[], int n) {
         }
         return result;
     }
+
+    // 744. Find Smallest Letter Greater Than Target
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0, end = letters.length - 1;
+        char result = letters[0];
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(letters[mid] == target){
+                if(mid + 1 < (letters.length - 1)){
+                    return letters[mid+1];
+                }else return letters[mid];
+            }
+            else if(letters[mid] > target){
+                result = letters[mid];
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
 }
