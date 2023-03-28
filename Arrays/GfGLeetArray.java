1efc2fba50774b63c2f908cdb8bb496d43b9cb9a
Aniket <aniketmessi3@gmail.com>
2023-03-28 23:05:36 +0530

    27. Remove Element

--- Arrays/GfGLeetArray.java
+++ Arrays/GfGLeetArray.java
@@ -227,4 +227,17 @@ static int findKRotation(int arr[], int n) {
     }
 // Bitonic arrray have similar approach as the peak element array. so questions like :
     /* "Maximum in a bitonic array and search an element in bitonic array all have same concept" */
+    // 27. Remove Element
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+        }
+    }
+    return i;
+}
 }
