a721e94a7981d9eba78d7d4ad5b42ca1422721ab
Aniket <aniketmessi3@gmail.com>
2023-04-01 22:36:36 +0530

    645. set Mismatch

--- Arrays/GfGLeetArray.java
+++ Arrays/GfGLeetArray.java
@@ -308,5 +308,27 @@ static boolean allocationPossible(int mid, int [] Arr, int M){
         }
         return res;
     }
-    // order
+    // 645. set Mismatch
+    public int[] findErrorNums(int[] nums) {
+        int[] arr = new int[2];
+        int i=0;
+        while (i < nums.length){
+            int correct_index = nums[i] - 1;
+            if(nums[i] != nums[correct_index]){
+                int temp = nums[correct_index];
+                nums[correct_index] = nums[i];
+                nums[i] = temp;
+            }else i++;
+
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j] != (j+1)){
+                arr[0] = nums[j];
+                arr[1] = j+1;
+                break;
+            }
+
+        }
+        return arr;
+    }
 }
