8a9d99318fd721230b6ef64038c7e3eac1b7e1f7
Aniket <aniketmessi3@gmail.com>
2023-04-05 00:55:06 +0530

    largestRectangleAreas simple approach..

--- Stacks&Queues/StackLeetGfg.java
+++ Stacks&Queues/StackLeetGfg.java
@@ -160,4 +160,46 @@ public class StackLeetGfg {
         System.out.println(Arrays.toString(area));
         return max_area;
     }
+    // simple approach
+    static int largestRectangleAreas(int[] heights) {
+        int n = heights.length;
+        int max = 0;
+        int nr[] = new int[n];
+        int nl[] = new int[n];
+
+        Stack<Integer> s = new Stack<>();
+        // Right
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.empty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+            if (s.empty()) {
+                nr[i] = n;
+            } else {
+                nr[i] = s.peek();
+            }
+            s.push(i);
+        }
+        // Left
+        s = new Stack<>();
+        for (int i = 0; i < heights.length; i++) {
+            while (!s.empty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+            if (s.empty()) {
+                nl[i] = -1;
+            } else {
+                nl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int hi = heights[i];
+            int width = nr[i] - nl[i] - 1;
+            int curr = hi * width;
+            max = Math.max(curr, max);
+        }
+        return max;
+    }
 }
